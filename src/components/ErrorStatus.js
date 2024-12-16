f67a7739d9e8714940be3961eeb914ffdafcdc07
Michal <pozdal.michal@gmail.com>
2024-12-16 02:02:21 +0100

    build

--- src/components/ErrorStatus.js
+++ src/components/ErrorStatus.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 import { useError } from '../contexts/ErrorContext';
 function ErrorStatus() {
 	const { error } = useError();
+	const [isVisible, setIsVisible] = useState(true);
+
 	return (
-		error?.message && (
-			<div className=" bg-red-500 p-4 text-white text-sm w-full font-bold z-10 absolute bottom-0">
+		error?.message &&
+		isVisible && (
+			<div className=" bg-red-500 p-4 flex justify-between items-center text-white text-sm w-full font-bold z-10 absolute bottom-0">
 				ERROR: {error?.message}
+				<button
+					onClick={() => {
+						setIsVisible(false);
+					}}
+				>
+					<i class="fa-solid fa-x"></i>
+				</button>
 			</div>
 		)
 	);
